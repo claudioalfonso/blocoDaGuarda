cc2a0374f8282180472b1429d2a2a9e7051991dc
pedrocortez <pedro.cortez@olxbr.com>
2017-01-25 01:32:02 -0200

    Add Fabric crashlytics and initialized

--- BlocoDaGuarda/analytics/src/main/java/com/generonumero/blocodaguarda/analytics/BDGTracking.java
+++ BlocoDaGuarda/analytics/src/main/java/com/generonumero/blocodaguarda/analytics/BDGTracking.java
@@ -3,13 +3,17 @@ package com.generonumero.blocodaguarda.analytics;
 
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.firebase.analytics.FirebaseAnalytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class BDGTracking {
 
 
     public static void initialize(Context context) {
         FirebaseAnalytics.getInstance(context);
+        Fabric.with(context, new Crashlytics());
     }
 
 }
