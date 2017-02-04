d7931f8be41c9ac283da513b9fb47aa33c4a1736
Pedro Cortez <pedro.fellipe.cortez@gmail.com>
2017-02-04 16:27:06 -0200

    Add drawer menu

--- BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/MainActivity.java
+++ BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/MainActivity.java
@@ -1,13 +1,31 @@
 package com.generonumero.blocodaguarda;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @Bind(R.id.navigation)
+    NavigationView navigationView;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
     }
 }
