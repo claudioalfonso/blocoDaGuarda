619b3513f181978c0542211ac1d3ad97bf2267d0
pedrocortez <pedro.cortez@olxbr.com>
2017-02-11 17:15:15 -0200

    Button on save Contacts

--- BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/network/view/impl/NetworkFragment.java
+++ BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/network/view/impl/NetworkFragment.java
@@ -7,23 +7,35 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.generonumero.blocodaguarda.R;
 import com.generonumero.blocodaguarda.network.view.NetworkView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NetworkFragment extends Fragment implements NetworkView {
 
     @Bind(R.id.bdg_network_recycler)
     RecyclerView mRecyclerView;
 
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.network_frag, null);
-        ButterKnife.bind(view);
+        ButterKnife.bind(this, view);
         return view;
     }
+
+
+    @OnClick(R.id.bdg_network_save)
+    public void onClickSave(View v) {
+        Toast.makeText(getContext(), "Salvou", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
