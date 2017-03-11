52371922c6dc7fb4e7b6aa2801292af9dc944074
Pedro Cortez <pedro.fellipe.cortez@gmail.com>
2017-03-11 18:13:11 -0300

    sending push

--- BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/push/PushMessagingService.java
+++ BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/push/PushMessagingService.java
@@ -1,5 +1,15 @@
 package com.generonumero.blocodaguarda.push;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+
+import com.generonumero.blocodaguarda.R;
+import com.generonumero.blocodaguarda.login.view.impl.LoginActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -8,5 +18,30 @@ public class PushMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+    }
+
+    @Override
+    public void zzm(Intent intent) {
+        super.zzm(intent);
+        Intent launchIntent = new Intent(this, LoginActivity.class);
+        launchIntent.setAction(Intent.ACTION_MAIN);
+        launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent,
+                PendingIntent.FLAG_ONE_SHOT);
+        Bitmap rawBitmap = BitmapFactory.decodeResource(getResources(),
+                R.mipmap.ic_launcher);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.icon_push)
+                .setLargeIcon(rawBitmap)
+                .setContentTitle(intent.getStringExtra("gcm.notification.title"))
+                .setContentText(intent.getStringExtra("gcm.notification.body"))
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+
     }
 }
