cbb7b6e614176ffadedf5e2ef8937887103a1e61
Pedro Cortez <pedro.fellipe.cortez@gmail.com>
2017-03-12 11:46:32 -0300

    add

--- BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/push/PushMessagingService.java
+++ BlocoDaGuarda/app/src/main/java/com/generonumero/blocodaguarda/push/PushMessagingService.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.generonumero.blocodaguarda.R;
 import com.generonumero.blocodaguarda.login.view.impl.LoginActivity;
@@ -14,14 +15,58 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class PushMessagingService extends FirebaseMessagingService {
+    private String TAG = "teste";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " +
+                remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+
+    }
+
+    public PushMessagingService() {
+        super();
+        Log.i(TAG, "PushMessagingService");
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+        Log.i(TAG, "onDeletedMessages");
+    }
+
+    @Override
+    public void onMessageSent(String s) {
+        super.onMessageSent(s);
+        Log.i(TAG, "onMessageSent");
+        Log.i(TAG, s);
+    }
+
+    @Override
+    public void onSendError(String s, Exception e) {
+        super.onSendError(s, e);
+        Log.i(TAG, "onSendError");
+        Log.i(TAG, s);
+        Log.i(TAG, "exce " +e.getMessage());
+    }
+
+    @Override
+    protected Intent zzF(Intent intent) {
+        Log.i(TAG, "zzF");
+        return super.zzF(intent);
+    }
+
+    @Override
+    public boolean zzH(Intent intent) {
+        Log.i(TAG, "zzH");
+        return super.zzH(intent);
     }
 
     @Override
     public void zzm(Intent intent) {
-        super.zzm(intent);
+        Log.i(TAG, "zzm");
         Intent launchIntent = new Intent(this, LoginActivity.class);
         launchIntent.setAction(Intent.ACTION_MAIN);
         launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
